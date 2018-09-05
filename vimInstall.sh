0f37a7d5de19b4a51cc841986d2f47f2eea99b3e
amuetter <andreas.muetter@ph.tum.de>
2018-09-05 14:15:58 +0200

    include fugitive in installation process

--- vimInstall.sh
+++ vimInstall.sh
@@ -17,6 +17,7 @@ cd ~/.vim/bundle
 git clone https://github.com/scrooloose/nerdtree.git
 git clone https://github.com/vim-scripts/wombat256.vim
 git clone https://github.com/vim-airline/vim-airline
+git clone https://github.com/tpope/vim-fugitive
 
 cd ~
 
