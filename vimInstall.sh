b8a6cb16be42f2b3f1a5977c4f5494d426b94b67
amuetter <andreas.muetter@ph.tum.de>
2019-05-09 09:45:53 +0200

    add linter

--- vimInstall.sh
+++ vimInstall.sh
@@ -20,7 +20,7 @@ git clone https://github.com/vim-airline/vim-airline
 git clone https://github.com/tpope/vim-fugitive
 git clone https://github.com/lifepillar/vim-solarized8
 git clone https://github.com/rkulla/pydiction.git
-
+git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
 cd ~
 
 ln -s ./rcfiles/.vimrc .vimrc
